29d0fd1636393a4f2968fb85808950e3317d3d32
Stefanos Baziotis <sdi1600105@di.uoa.gr>
2023-10-12 18:46:13 -0500

    [PattMatcher] Add missing patterns in the return types

--- dias/patt_matcher.py
+++ dias/patt_matcher.py
@@ -1460,6 +1460,7 @@ Union[
   HasSubstrSearchApply,
   IsInplaceUpdate,
   HasToListConcatToSeries,
+  ApplyVectorizedLambda,
   RemoveAxis1Lambda,
   IsTrivialDFCall,
   IsTrivialDFAttr,
@@ -1469,6 +1470,7 @@ Union[
   ReplaceRemoveList,
   MultipleStrInCol,
   FuseIsIn,
+  FuseApply,
   StrSplitPython,
   StrAttrIndexed,
   SubToSubReplace
