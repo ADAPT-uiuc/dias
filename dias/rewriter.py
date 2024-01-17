1b9bb6e7ba084cd0be08cc98c8970db4279049c5
kaushik327 <kaushik.r.varadharajan@gmail.com>
2024-01-16 22:59:57 -0600

    remove commented-out variable

--- dias/rewriter.py
+++ dias/rewriter.py
@@ -1590,7 +1590,6 @@ dias.rewriter.rewrite("{verbose}",
   return res
 
 ip = get_ipython()
-# os.environ["_IREWR_USE_AS_LIB"] = "True"
 use_as_lib = "_IREWR_USE_AS_LIB" in os.environ and os.environ["_IREWR_USE_AS_LIB"] == "True"
 if ip is None or use_as_lib:
   text = "IPython instance has not been detected. Dias won't rewrite cells \
